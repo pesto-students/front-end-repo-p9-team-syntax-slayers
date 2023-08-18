4dc70a3d9ef3cfc1dc536b2b2bb33c814071abef
Sidhanth Kamble <sidhanthkamble@Sidhanths-MacBook-Pro.local>
2023-08-19 00:25:54 +0530

    final page formatted

--- saloni/src/pages/finalSelection/FinalSelection.tsx
+++ saloni/src/pages/finalSelection/FinalSelection.tsx
@@ -1,19 +1,37 @@
-import { HStack,Text, Flex, useDisclosure,Heading,useBreakpointValue,Modal,ModalFooter,ModalHeader,ModalOverlay,ModalBody,ModalCloseButton ,ModalContent,Box, Divider, Button } from "@chakra-ui/react";
-import SelectedServiceCard ,{SelectedServiceCardProps} from "../../components/SelectedServiceCard/SelectedServiceCard";
-import TotalCostAndDetails from "../../components/TotalCostAndDetails/TotalCostAndDetails";
-import DateSlots from "../../components/DateSlots/DateSlots";
-import React, { useEffect, useState } from "react";
-import { SelectedServiceDummyList } from "../../components/SelectedServiceCard/Helper";
-import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import { DateAndTime } from "../../components/DateSlots/DateSlots";
-import { emptyCart } from "../../redux/slices/cart";
+import {
+  HStack,
+  Text,
+  Flex,
+  useDisclosure,
+  Heading,
+  useBreakpointValue,
+  Modal,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  Box,
+  Divider,
+  Button,
+} from '@chakra-ui/react';
+import SelectedServiceCard, {
+  SelectedServiceCardProps,
+} from '../../components/SelectedServiceCard/SelectedServiceCard';
+import TotalCostAndDetails from '../../components/TotalCostAndDetails/TotalCostAndDetails';
+import DateSlots from '../../components/DateSlots/DateSlots';
+import React, { useEffect, useState } from 'react';
+import { SelectedServiceDummyList } from '../../components/SelectedServiceCard/Helper';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { DateAndTime } from '../../components/DateSlots/DateSlots';
+import { emptyCart } from '../../redux/slices/cart';
 const FinalSelection = () => {
-  
-  const view = useBreakpointValue({ base: "360px", sm: "400px" })
-  const navigate = useNavigate()
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const view = useBreakpointValue({ base: '360px', sm: '400px' });
+  const navigate = useNavigate();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const [selectedServiceList, setSelectServiceList] = useState<
     SelectedServiceCardProps[]
@@ -21,10 +39,10 @@ const FinalSelection = () => {
   const cart = useAppSelector((state) => state.cart);
   const user = useAppSelector((state) => state.user);
   const dispatch = useAppDispatch();
-  const [totalTime, setTotalTime] = useState(0)
-  const [finalSlots, setFinalSlots] = useState<DateAndTime | null>()
-  const [message, setMessage] = useState('')
-  const [loading, setLoading] = useState(false)
+  const [totalTime, setTotalTime] = useState(0);
+  const [finalSlots, setFinalSlots] = useState<DateAndTime | null>();
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     console.log(cart.cartList);
@@ -46,95 +64,90 @@ const FinalSelection = () => {
     // Use the data as needed
   };
 
-
-  const checkoutHandler = async()=>{
-    setLoading(true)
+  const checkoutHandler = async () => {
+    setLoading(true);
     //This API is used to create OrderId
     // const orderData = await axios.post(`${process.env.REACT_APP_BASEURL}${process.env.REACT_APP_RAZORPAY_CHECKOUT}`,{
     //   amount:400
     // })
-  //   const payload = {
-  //     "userId": user.userId,
-  //     "salonId": cart.salonId,
-  //     "serviceIds": cart.cartList.map(service => service.id),
-  //     "orderId": orderData,
-  //     "timeAndDateSlots": finalSlots
-  // }
+    //   const payload = {
+    //     "userId": user.userId,
+    //     "salonId": cart.salonId,
+    //     "serviceIds": cart.cartList.map(service => service.id),
+    //     "orderId": orderData,
+    //     "timeAndDateSlots": finalSlots
+    // }
 
-  const payload1 ={
-    "userId": user.userId,
-   "serviceIds": cart.cartList.map(service => service.id),
-    "salonId":cart.salonId,
-    "finalDates":finalSlots
-   
- }  
- console.log(payload1)
- const headers = {
-  'Authorization': `Bearer ${user.token}`, 
-  'Content-Type': 'application/json',
-}; 
- const apiEndpointBooking = `${process.env.REACT_APP_BASEURL}${process.env.REACT_APP_BOOK_SERVICE}${user.userId}`
- axios.post(apiEndpointBooking,payload1,{headers})
- .then((res)=>{
-  console.log(res)
-  setMessage(res.data.message)
-  setLoading(false)
-  onOpen()
+    const payload1 = {
+      userId: user.userId,
+      serviceIds: cart.cartList.map((service) => service.id),
+      salonId: cart.salonId,
+      finalDates: finalSlots,
+    };
+    console.log(payload1);
+    const headers = {
+      Authorization: `Bearer ${user.token}`,
+      'Content-Type': 'application/json',
+    };
+    const apiEndpointBooking = `${process.env.REACT_APP_BASEURL}${process.env.REACT_APP_BOOK_SERVICE}${user.userId}`;
+    axios
+      .post(apiEndpointBooking, payload1, { headers })
+      .then((res) => {
+        console.log(res);
+        setMessage(res.data.message);
+        setLoading(false);
+        onOpen();
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      });
 
- })
- .catch((err)=>{
-  setLoading(false)
-  console.log(err)
- })
+    // console.log(payload)
+    //  const {data} = orderData
+    //  console.log(`${process.env.REACT_APP_BASEURL as string}${process.env.REACT_APP_RAZORPAY_VERIFICATION as string}`)
+    //   const options = {
+    //           key: `${process.env.REACT_APP_RAZORPAY_API_KEY as string}`,
+    //           amount: data.amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    //           currency: "INR",
+    //           name: "Saloni",
+    //           description: "Test Transaction",
+    //           image: "/logo-black.png",
+    //           order_id: data.data.id,
+    //           callback_url: `${process.env.REACT_APP_BASEURL as string}${process.env.REACT_APP_RAZORPAY_VERIFICATION as string}`,
+    //           prefill: {
+    //               name: "Gaurav Kumar",
+    //               email: "gaurav.kumar@example.com",
+    //               contact: "9000090000"
+    //           },
+    //           notes: {
+    //               "address": "Razorpay Corporate Office"
+    //           },
+    //           theme: {
+    //               "color": "#242C2F"
+    //           }
+    //     };
+    //     console.log('Order Data from BE:', data.data);
+    //     console.log('Razorpay Options:', options);
 
-  // console.log(payload)
-  //  const {data} = orderData
-  //  console.log(`${process.env.REACT_APP_BASEURL as string}${process.env.REACT_APP_RAZORPAY_VERIFICATION as string}`)
-  //   const options = {
-  //           key: `${process.env.REACT_APP_RAZORPAY_API_KEY as string}`,
-  //           amount: data.amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-  //           currency: "INR",
-  //           name: "Saloni",
-  //           description: "Test Transaction",
-  //           image: "/logo-black.png",
-  //           order_id: data.data.id, 
-  //           callback_url: `${process.env.REACT_APP_BASEURL as string}${process.env.REACT_APP_RAZORPAY_VERIFICATION as string}`,
-  //           prefill: {
-  //               name: "Gaurav Kumar",
-  //               email: "gaurav.kumar@example.com",
-  //               contact: "9000090000"
-  //           },
-  //           notes: {
-  //               "address": "Razorpay Corporate Office"
-  //           },
-  //           theme: {
-  //               "color": "#242C2F"
-  //           }
-  //     };
-  //     console.log('Order Data from BE:', data.data);
-  //     console.log('Razorpay Options:', options);
-      
-  //           const rzp1 = new (window as any).Razorpay(options);     
-  //           rzp1.open();
+    //           const rzp1 = new (window as any).Razorpay(options);
+    //           rzp1.open();
+  };
 
-            }
-  
-            const handlePostBooking=()=>{
-               
-              let apiEndpoint=`${process.env.REACT_APP_BASEURL}${process.env.REACT_APP_CLEAR_CART}`
-              axios.post(apiEndpoint,{"userId":user.userId})
-              .then((res)=>{
-                console.log(res)
-                dispatch(emptyCart())
-              })
-              .catch((error)=>console.log(error))
+  const handlePostBooking = () => {
+    let apiEndpoint = `${process.env.REACT_APP_BASEURL}${process.env.REACT_APP_CLEAR_CART}`;
+    axios
+      .post(apiEndpoint, { userId: user.userId })
+      .then((res) => {
+        console.log(res);
+        dispatch(emptyCart());
+      })
+      .catch((error) => console.log(error));
 
-              dispatch(emptyCart())
-              onClose()   
-              navigate('/')
-
-            }
-  
+    dispatch(emptyCart());
+    onClose();
+    navigate('/');
+  };
 
   return (
     <>
@@ -195,29 +208,54 @@ const FinalSelection = () => {
       </Box>
       <Divider />
 
-      <Box padding={{base:"5",sm:"10"}} pb={{sm:"0"}}>
-      <HStack>
-       {view=="400px" &&   <DateSlots  onDateSlotSelected={handleSelectedDateSlots} salonId={'1'} totalTime={totalTime} />} 
-      
-       <Button isLoading={loading} onClick={checkoutHandler} width={{base:'100%', md:"50%",sm:"10%"}} h={{base:"40px",sm:"70px"}} variant={useBreakpointValue({ base: "solid", sm: "outline" })} bg={{ base: "accent.500", sm: "white" }} color={{ base: "white", sm: "accent.500" }} colorScheme={useBreakpointValue({ base: "accent.500", sm: "white" })} mb={{base:"5",sm:"10"}}>Book Now</Button>
-       </HStack>
+      <Box padding={{ base: '5', sm: '10' }} pb={{ sm: '0' }}>
+        <HStack>
+          {view == '400px' && (
+            <DateSlots
+              onDateSlotSelected={handleSelectedDateSlots}
+              salonId={'1'}
+              totalTime={totalTime}
+            />
+          )}
 
-       <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Booking Status</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            Your Service Booking is {message} !
-          </ModalBody>
+          <Button
+            isLoading={loading}
+            onClick={checkoutHandler}
+            width={{ base: '100%', md: '50%', sm: '10%' }}
+            h={{ base: '40px', sm: '70px' }}
+            variant={useBreakpointValue({ base: 'solid', sm: 'outline' })}
+            bg={{ base: 'accent.500', sm: 'white' }}
+            color={{ base: 'white', sm: 'accent.500' }}
+            colorScheme={useBreakpointValue({
+              base: 'accent.500',
+              sm: 'white',
+            })}
+            mb={{ base: '5', sm: '10' }}
+          >
+            Book Now
+          </Button>
+        </HStack>
 
-          <ModalFooter>
-            <Button color={'accent.500'} variant={'outline'} colorScheme='accent.500' mr={3} onClick={handlePostBooking} >
-              Go To Home
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Booking Status</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>Your Service Booking is {message} !</ModalBody>
+
+            <ModalFooter>
+              <Button
+                color={'accent.500'}
+                variant={'outline'}
+                colorScheme="accent.500"
+                mr={3}
+                onClick={handlePostBooking}
+              >
+                Go To Home
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </Box>
     </>
   );
