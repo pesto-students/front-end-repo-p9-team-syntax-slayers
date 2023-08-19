e7bdd24c9bdec344f439252904ac46d43f609e85
Chethan Rao S <chethan@medflix.app>
2023-08-19 09:40:12 +0530

    Salon Card Modofoed

--- saloni/src/components/SalonCard/SalonCard.tsx
+++ saloni/src/components/SalonCard/SalonCard.tsx
@@ -9,17 +9,17 @@ import {
   Image,
   Text,
   Button,
+  VStack,
 } from '@chakra-ui/react';
 import { StarIcon } from '@chakra-ui/icons';
 import { GiPathDistance } from 'react-icons/gi';
 import { GrLocation } from 'react-icons/gr';
 import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from "../../redux/hooks/index";
+import { useAppDispatch, useAppSelector } from '../../redux/hooks/index';
 
 import GenderBtn from '../Buttons/GenderBtn';
 import { addSalonDetails } from '../../redux/slices/cart';
-
 
 export interface SalonCardProps {
   id?: string;
@@ -47,21 +47,17 @@ const SalonCard: React.FC<SalonCardProps> = (props) => {
   } = props;
 
   const history = useNavigate();
-  const dispatch = useAppDispatch()
-  const user = useAppSelector(state=>state.user)
+  const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.user);
 
   const handleCardClick = () => {
-      
-      if(user.isLoggedIn){
-        const salon={salonName,location,gender,imageUrl}
-        dispatch(addSalonDetails(salon))
-        history(`/salonDetails/${id}`);
-      }
-      else{
-        alert('please login')
-      }
-
-      
+    if (user.isLoggedIn) {
+      const salon = { salonName, location, gender, imageUrl };
+      dispatch(addSalonDetails(salon));
+      history(`/salonDetails/${id}`);
+    } else {
+      alert('please login');
+    }
   };
 
   return (
@@ -122,36 +118,35 @@ const SalonCard: React.FC<SalonCardProps> = (props) => {
           )}
         </Flex>
 
-        <CardBody>
-          <Flex
+        <CardBody p={'8px'}>
+          <VStack
             direction={{ base: 'row', md: 'row' }}
-            alignItems={{ base: 'flex-start', md: 'center' }}
+            alignItems={{ base: 'flex-start' }}
           >
-            <Box flex="1">
+            <Box flex="1" maxW={'90%'}>
               <Heading as="h6" size="sm">
                 {salonName}
               </Heading>
               <HStack>
                 <HStack mr="3px">
                   <StarIcon color="gold" />
-                  <Text fontSize="sm">{rating}</Text>
+                  <Text fontSize="sm">{Number(rating).toFixed(2)}</Text>
                   <Text fontSize="sm">({props.ratingCount})</Text>
                 </HStack>
 
                 <HStack ml={['10px', '18px']}>
                   <GiPathDistance />
                   <Text fontSize="sm">{distance} Kms</Text>
+                  <GenderBtn> {gender} </GenderBtn>
                 </HStack>
-
-                <GenderBtn> {gender} </GenderBtn>
-              </HStack>
-
-              <HStack>
-                <GrLocation />
-                <Text fontSize="sm">{location}</Text>
               </HStack>
             </Box>
-          </Flex>
+            <HStack>
+              {' '}
+              <GrLocation />
+              <Text fontSize="sm">{location}</Text>
+            </HStack>
+          </VStack>
         </CardBody>
       </Card>
     </motion.div>
